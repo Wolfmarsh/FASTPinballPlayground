23de23a59e2e906933c391ff654cc4cfe795a9b9
C. Benn <cfbenn@gmail.com>
2015-10-21 23:51:52 -0400

    DMD Port
    New Class

--- FASTHardware/My Project/AssemblyInfo.vb
+++ FASTHardware/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("dfea5adb-87f6-43ec-9512-76d635198eb2")> 
+<Assembly: Guid("dfea5adb-87f6-43ec-9512-76d635198eb2")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.2")> 
-<Assembly: AssemblyFileVersion("1.0.0.2")> 
+<Assembly: AssemblyVersion("1.0.0.3")>
+<Assembly: AssemblyFileVersion("1.0.0.3")>
--- FASTPinballTestPlayground/My Project/AssemblyInfo.vb
+++ FASTPinballTestPlayground/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("6091393f-5897-48f2-9b82-f70830b49351")> 
+<Assembly: Guid("6091393f-5897-48f2-9b82-f70830b49351")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.2")> 
-<Assembly: AssemblyFileVersion("1.0.0.2")> 
+<Assembly: AssemblyVersion("1.0.0.3")>
+<Assembly: AssemblyFileVersion("1.0.0.3")>
